4a2309dcf33d0e4638d2042e636ee7437f7656b5
Jes5s <JsslDssn@gmail.com>
2024-11-28 23:38:54 +0800

    Update web_app.js

--- web_app.js
+++ web_app.js
@@ -20,3 +20,15 @@ db.serialize(() => {
 });
 
 module.exports = db;
+
+router.post('/', (req, res) => {
+    const { name, description } = req.body;
+    const query = `INSERT INTO items (name, description) VALUES (?, ?)`;
+    db.run(query, [name, description], function (err) {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        } else {
+            res.status(201).json({ id: this.lastID });
+        }
+    });
+});

a577516964842b0e476c45e2532a90688b1ac0fe
Jes5s <JsslDssn@gmail.com>
2024-12-05 13:30:09 +0800

    del

--- web-app/app.js
+++ web-app/app.js
@@ -1,2 +1,20 @@
-const tasksRoutes = require('./routes/tasks');
-app.use('/', tasksRoutes);
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const todoRoutes = require('./routes/todos');
+
+app.use('/todos', todoRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the To-Do List App!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
